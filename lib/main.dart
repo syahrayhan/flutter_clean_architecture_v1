e4ee589702a4f8dd6a785bc48bd39da8f3a9a214
Rayhan <syahrayhanid@gmail.com>
2022-01-17 16:47:40 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:clean_architecture/src/config/routes/app_routes.dart';
+import 'package:clean_architecture/src/config/themes/app_theme.dart';
+import 'package:clean_architecture/src/core/utils/constants.dart';
+import 'package:flutter/material.dart';
+
+Future<void> main() async {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: kMaterialAppTitle,
+      onGenerateRoute: AppRoutes.onGenerateRoutes,
+      theme: AppTheme.light,
+    );
+  }
+}
---
+++ lib/src/config/themes/app_theme.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static ThemeData get light {
+    return ThemeData(
+      appBarTheme: const AppBarTheme(
+        elevation: 0,
+        color: Colors.white,
+      ),
+      scaffoldBackgroundColor: Colors.white,
+      primaryColor: Colors.black,
+      colorScheme: const ColorScheme.light(
+        secondary: Colors.black,
+      ),
+      splashColor: Colors.transparent,
+    );
+  }
+}
